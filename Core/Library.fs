7f2c8590382677ad616d87596751346d63f5a1f6
Allan Menoret <allan.menoret@gmail.com>
2019-12-05 17:47:15 +0100

    part 1 done

--- Core/Library.fs
+++ Core/Library.fs
@@ -1,5 +1,56 @@
 ﻿namespace Core
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+
+module CableFix =
+    type Direction = Right | Left | Up | Down
+
+    type Move = {direction: Direction; step: int}
+
+    type Position = {x:int; y:int}
+
+    let (|StartWith|_|) (p:string) (s:string) =
+        if s.StartsWith(p) then
+            Some(s.Substring(p.Length))
+        else
+            None
+
+    let getCablesPosition (cables: string) =
+        let result = cables.Split [|'\n'|] |> Seq.map(fun cable -> cable.Split [|','|])
+        (Seq.head result, Seq.last result)
+
+    let extract (input: string) =
+        match input.[0] with
+            | 'R' -> Some {direction= Right; step=input.[1..] |> int}
+            | 'L' -> Some {direction= Left; step=input.[1..] |> int}
+            | 'D' -> Some {direction= Down; step=input.[1..] |> int}
+            | 'U' -> Some {direction= Up; step=input.[1..] |> int}
+            | _ -> None
+
+    let dirToIteration (dir: Direction) =
+        match dir with
+            | Right -> (1, 0)
+            | Left -> (-1, 0)
+            | Down -> (0, -1)
+            | Up  -> (0, 1)
+
+    let loop (x1, y1) ((x, y), step) =
+        if (step = 0) then None
+        else Some({x=x+x1; y=y+y1}, ((x+x1, y+y1), step - 1))
+    
+    let generateCoord (coords:seq<seq<Position>>) (move: Move) =
+        let iteration = dirToIteration move.direction
+        let last = coords |> Seq.last |> Seq.last
+        let generatePosition = loop iteration
+        let news = Seq.unfold generatePosition ((last.x, last.y), move.step)
+        Seq.append coords (seq {news})
+    
+    let findPosition (cable: seq<string>) =
+        cable |> Seq.choose(extract) |> Seq.fold generateCoord (seq {seq {{x=0;y=0}}}) |> Seq.concat
+
+    let manhattanDistance (cables: string) =
+        let (cable1, cable2) = cables |> getCablesPosition
+        let cable1Positions = cable1 |> findPosition |> Set.ofSeq
+        let cable2Positions = cable2 |> findPosition |> Set.ofSeq
+        let distances = cable1Positions |> Set.intersect cable2Positions
+        let result = distances |> Set.toSeq |> Seq.map (fun pos -> abs pos.x + abs pos.y) |> Seq.filter (fun dist -> dist <> 0) |> Seq.min
+        result
--- Tests/Tests.fs
+++ Tests/Tests.fs
@@ -1,8 +1,52 @@
 module Tests
 
-open System
 open Xunit
+open FsUnit
+open Core.CableFix
+
+[<Theory>]
+[<InlineData("R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83", 159)>]
+[<InlineData("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7", 135)>]
+let ``Get manhattan distance`` (cables:string, expectedDistance:int) =
+    cables |> manhattanDistance |> should equal expectedDistance
+
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Get cables`` () =
+    let (cable1, cable2) = "R75,D30,D30,U1\nD58" |> getCablesPosition
+    cable1 |> should equivalent ["R75";"D30";"D30";"U1"]
+    cable2 |> should equivalent ["D58"]
+
+[<Fact>]
+let ``Extract coord R75`` () =
+    "R75" |> extract |> should equal <| Some {direction= Right; step= 75}
+
+[<Fact>]
+let ``Extract coord L1`` () =
+    "L1" |> extract |> should equal <| Some {direction= Left; step= 1}
+
+[<Fact>]
+let ``Extract coord U42`` () =
+    "U42" |> extract |> should equal <| Some {direction= Up; step= 42}
+
+[<Fact>]
+let ``Extract coord D129`` () =
+    "D129" |> extract |> should equal <| Some {direction= Down; step= 129}
+
+
+[<Fact>]
+let ``dirToIteration Right`` () =
+    Right |> dirToIteration |> should equal (1, 0)
+
+[<Fact>]
+let ``dirToIteration Left`` () =
+    Left |> dirToIteration |> should equal (-1, 0)
+
+[<Fact>]
+let ``dirToIteration Up`` () =
+    Up |> dirToIteration |> should equal (0, 1)
+
+[<Fact>]
+let ``dirToIteration Down`` () =
+    Down |> dirToIteration |> should equal (0, -1)
+
